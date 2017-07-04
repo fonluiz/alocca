2efc1b3ac0a1795abceee334eaaa7e34af6cec21
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2017-07-04 00:15:55 -0300

    Create semester model

---
+++ projeto/src/app/semesters/semester.model.ts
@@ -0,0 +1,9 @@
+﻿import { Allocation } from '../allocations/allocation.model'
+
+export class Semester {
+    constructor(
+        public semester_id: string,
+        public allocations: Allocation[]
+    ) { }
+
+}
