6303362ab46c860033bf39b331c4f54f259d7679
fonluiz <luiz.fonseca@ccc.ufcg.edu.br>
2017-07-05 22:03:54 -0300

    Add buttons for selecting tough and tractabe professors restrictions

--- projeto/src/app/professors/add-restriction/add-restriction.component.ts
+++ projeto/src/app/professors/add-restriction/add-restriction.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+ 
 @Component({
   selector: 'app-add-restriction',
   templateUrl: './add-restriction.component.html',
@@ -11,9 +11,28 @@ export class AddRestrictionComponent implements OnInit {
   hours = ["7:00 - 8:00", "8:00 - 10:00", "10:00 - 12:00", "12:00 - 14:00",
           "14:00 - 16:00", "16:00 - 18:00", "18:00 - 20:00", "20:00 - 22:00"];
 
+  toughRestrictionSliderValue = true;
+  tractableRestrictionSliderValue = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public onToughRestrictionsSliderToggled(event) {
+    if (event.checked){
+        this.tractableRestrictionSliderValue = false;
+    } else {
+        this.tractableRestrictionSliderValue = true;
+    }
+  }
+
+  public onTractableRestrictionsSliderToggled(event) {
+    if (event.checked){
+        this.toughRestrictionSliderValue = false;
+    } else {
+        this.toughRestrictionSliderValue = true;
+    }
   }
 
 }
