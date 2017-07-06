82811512449fbc3aac8c6c9441afc00aa13f82be
fonluiz <luiz.fonseca@ccc.ufcg.edu.br>
2017-07-06 00:21:46 -0300

    Delete the slide toggles for selecting the type of schedule rstriction

--- projeto/src/app/professors/add-restriction/add-restriction.component.ts
+++ projeto/src/app/professors/add-restriction/add-restriction.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
  
+import {MdCheckbox} from '@angular/material';
 @Component({
   selector: 'app-add-restriction',
   templateUrl: './add-restriction.component.html',
@@ -11,28 +12,16 @@ export class AddRestrictionComponent implements OnInit {
   hours = ["7:00 - 8:00", "8:00 - 10:00", "10:00 - 12:00", "12:00 - 14:00",
           "14:00 - 16:00", "16:00 - 18:00", "18:00 - 20:00", "20:00 - 22:00"];
 
-  toughRestrictionSliderValue = true;
-  tractableRestrictionSliderValue = false;
-
   constructor() { }
 
   ngOnInit() {
   }
 
-  public onToughRestrictionsSliderToggled(event) {
-    if (event.checked){
-        this.tractableRestrictionSliderValue = false;
-    } else {
-        this.tractableRestrictionSliderValue = true;
+  private testTable() {
+    for (var i = 1; i <= 40; i++) {
+      var x = <MdCheckbox><any>document.getElementById("input-md-checkbox-"+i);
     }
-  }
-
-  public onTractableRestrictionsSliderToggled(event) {
-    if (event.checked){
-        this.toughRestrictionSliderValue = false;
-    } else {
-        this.toughRestrictionSliderValue = true;
-    }
+    console.log(x.checked);
   }
 
 }
