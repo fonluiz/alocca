610e6377e781d8684af8999b0ceebdb782bdba01
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2017-08-06 21:17:45 -0300

    Arrange imports and create constants correctly

--- projeto/src/app/app.component.ts
+++ projeto/src/app/app.component.ts
@@ -1,17 +1,22 @@
 ﻿import { Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from 'angularfire2/auth';
-import * as firebase from 'firebase/app';
-import { FlashMessagesService } from 'angular2-flash-messages';
 import { Router } from '@angular/router';
-import { FirebaseService } from './services/firebase.service';
+
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+import * as firebase from 'firebase/app';
 
 import { Observable } from 'rxjs/Observable';
 
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
-import { User } from './users/user.model';
 import { NavbarService } from './services/navbar.service';
-
 import { SnackbarService } from './services/snackbar.service';
+import { FirebaseService } from './services/firebase.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+
+import { User } from './users/user.model';
+
+const TIMEOUT_NOT_REGISTERED: number = 5000;
+const NOT_REGISTERED_MESSAGE: string = "Opa! Parece que você não está cadastrado. Entre em contato com o administrador.";
 
 @Component({
   selector: 'app-root',
@@ -20,8 +25,6 @@ import { SnackbarService } from './services/snackbar.service';
 })
 export class AppComponent {
   user: Observable<firebase.User>;
-  TIMEOUT_NOT_REGISTERED = 5000;
-  NOT_REGISTERED_MESSAGE: string = "Opa! Parece que você não está cadastrado. Entre em contato com o administrador.";
 
   constructor(
     public FBservice: FirebaseService,
@@ -51,7 +54,7 @@ export class AppComponent {
           console.log(userEmail);
           console.log('pegou o logout');
           //this._flashMessagesService.show(this.NOT_REGISTERED_MESSAGE, { cssClass: 'alert-danger', timeout: this.TIMEOUT_NOT_REGISTERED });
-          this.snackService.openSnackBar(this.NOT_REGISTERED_MESSAGE,this.TIMEOUT_NOT_REGISTERED);
+          this.snackService.openSnackBar(NOT_REGISTERED_MESSAGE, TIMEOUT_NOT_REGISTERED);
         }
       }
     });
