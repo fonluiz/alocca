99c1624e2d03a4dedab784e90677d6d05223d85b
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2017-08-20 00:02:00 -0300

    Add observation about alert creation

--- projeto/src/app/alerts/alert.factory.ts
+++ projeto/src/app/alerts/alert.factory.ts
@@ -1,6 +1,7 @@
 ﻿import { Alert } from "app/alerts/alert.model";
 
 export class AlertFactory {
+    // By default, an alert is non-ignored on creation.
     create(title: string, message: string) {
         return new Alert(title, message);
     }
