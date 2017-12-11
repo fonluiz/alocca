e00a384d796cb258861dae93d9f84d680aa70920
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2017-12-11 20:37:32 -0300

    Remove not used FBService

--- alocca/src/app/semesters/add-semester/add-semester.component.ts
+++ alocca/src/app/semesters/add-semester/add-semester.component.ts
@@ -1,5 +1,4 @@
 ﻿import { Component, OnInit } from '@angular/core';
-import { FirebaseService } from '../../services/firebase.service';
 import { SemestersDmService } from '../../data-manager/semesters/semesters-dm.service';
 import { SnackbarService } from '../../services/snackbar.service';
 import { Semester } from '../semester.model';
@@ -40,7 +39,6 @@ export class AddSemesterComponent implements OnInit {
     TIMEOUT_SAVED_MESSAGE: number = 2500;
 
     constructor(
-        private FBservice: FirebaseService,
         private semesterDmService: SemestersDmService,
         private snackService: SnackbarService
     ) {
