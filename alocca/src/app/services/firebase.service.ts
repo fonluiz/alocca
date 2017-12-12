2ba21e3e94705861e59294dffe3488077cf167d2
Gabriel <hugo.silva@ccc.ufcg.edu.br>
2017-12-12 01:17:21 -0300

    Temporarily resolves class addition problems Corrects the problem of class display

--- alocca/src/app/services/firebase.service.ts
+++ alocca/src/app/services/firebase.service.ts
@@ -76,54 +76,64 @@ export class FirebaseService {
   saveClass(classToSave: Class) {
     var classRef = this.db.database.ref(this.CLASSES_PATH + '/' + this.currentSemester + '/' + classToSave.getId());
     var detailedCourse: any;
-    this.getCourseDetails(classToSave.getCourse()).valueChanges().subscribe(course_ => {
-      detailedCourse = course_;
+    var self: FirebaseService = this;
+    var promise = new Promise (function(result, reject){
+        self.getCourseDetails(classToSave.getCourse()).valueChanges().subscribe(course_ => {
+        detailedCourse = course_;
+      })
     })
-    var recomendedSemester: string = "";
-    if(detailedCourse.minimumSemester!==detailedCourse.maximumSemester){
-      recomendedSemester = detailedCourse.minimumSemester+"-"+detailedCourse.maximumSemester;
-    }else{
-      recomendedSemester = detailedCourse.minimumSemester;
-    }
-    classRef.update({
-      "isVerified":false,
-      "recomendedSemester":recomendedSemester,
-      "course":detailedCourse.shortName,
-      "number":classToSave.getNumber(),
-      "professor1":"",
-      "professor2":"",
-      "schedules":{
-        "monday":{
-          "hours":""
-        },
-        "tuesday":{
-          "hours":""
-        },
-        "wednesday":{
-          "hours":""
-        },
-        "thursday":{
-          "hours":""
-        },
-        "friday":{
-          "hours":""
+
+    console.log("promise", promise);
+
+    promise.then(
+      function(){
+        var recomendedSemester: string = "";
+        if(detailedCourse.minimumSemester!==detailedCourse.maximumSemester){
+          recomendedSemester = detailedCourse.minimumSemester+"-"+detailedCourse.maximumSemester;
+        }else{
+          recomendedSemester = detailedCourse.minimumSemester;
         }
-      },
-      "hoursToSchedule": detailedCourse.hoursToSchedule,
-      "note":""
-    })
-    // Only saves the data if it does not exists already
-   classRef.once('value').then(
-      function(snapshot) {
-        if (snapshot.val() == null) {
-          classRef.set(classToSave.toFirebaseObject());
-          return true;
-        } else {
-          return false;
-        }
-      }
-    );
+        classRef.update({
+          "isVerified":false,
+          "recomendedSemester":recomendedSemester,
+          "course":detailedCourse.shortName,
+          "number":classToSave.getNumber(),
+          "professor1":"",
+          "professor2":"",
+          "schedules":{
+            "monday":{
+              "hours":""
+            },
+            "tuesday":{
+              "hours":""
+            },
+            "wednesday":{
+              "hours":""
+            },
+            "thursday":{
+              "hours":""
+            },
+            "friday":{
+              "hours":""
+            }
+          },
+          "hoursToSchedule": detailedCourse.hoursToSchedule,
+          "note":""
+        })
+      })
+        // Only saves the data if it does not exists already
+      classRef.once('value').then(
+          function(snapshot) {
+            if (snapshot.val() == null) {
+              classRef.set(classToSave.toFirebaseObject());
+              return true;
+            } else {
+              return false;
+            }
+          }
+        );
   }
+
   getClasses() {
     let classesList = this.db.list('/classes/'+this.currentSemester) as AngularFireList<any[]>;
     return classesList;
